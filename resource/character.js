230a6ee1f90b75ec12b358307b709ad138164e67
alog243 <alog243@gmail.com>
2023-10-08 17:11:33 -0400

    div title for mouseover

---
+++ resource/character.js
@@ -0,0 +1,47 @@
+function generate_character_div(character) {
+    let new_char_div = document.createElement('div');
+    new_char_div.classList.add('character-icon');
+    switch(character.star_rarity) {
+        case 3:
+            new_char_div.classList.add('three-star');
+            break;
+        case 4:
+            new_char_div.classList.add('four-star');
+            break;
+        case 5:
+            new_char_div.classList.add('five-star');
+            break;
+        default:
+            break;
+    }
+
+    let new_char_img = document.createElement('img');
+    if(character.alt_icons.length > 0 && Math.random() < 0.1) {
+        new_char_img.src = "resource/" + 
+            character.alt_icons[Math.floor(Math.random()*character.alt_icons.length)];
+    }
+    else {
+        new_char_img.src = "resource/" + character.icon;
+    }
+    new_char_img.alt = character.name;
+    new_char_img.classList.add('char-icon');
+    new_char_div.appendChild(new_char_img);
+    new_char_div.title = character.name;
+    
+
+    if (character.element != "none") {
+        let new_element_img = document.createElement('img');
+        new_element_img.src = "resource/Element_" + character.element.charAt(0).toUpperCase() + character.element.slice(1) + ".png";
+        new_element_img.classList.add('element-icon');
+        new_char_div.appendChild(new_element_img);
+    }
+    
+    if (character.role != "none") {
+        let new_role_img = document.createElement('img');
+        new_role_img.src = "resource/role-" + character.role + ".webp";
+        new_role_img.classList.add('role-icon');
+        new_char_div.appendChild(new_role_img);
+    }
+    
+    return new_char_div;
+}
